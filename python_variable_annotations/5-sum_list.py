cf6d9cf3d497f5198f90700bd5fe559ba477a70f
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-23 16:16:35 -0400

    fixing tasks

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Staticly typed list sum fucntion """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sums all float in a list """
     num: float = 0.0
     for i in input_list:
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Staticly typed mixed list sum fucntion """
+from typing import Union, List
 
-
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Sums a mixed list """
     num: float = 0
     for i in mxd_lst:
