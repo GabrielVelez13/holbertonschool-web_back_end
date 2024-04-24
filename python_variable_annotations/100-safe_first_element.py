3bfa41ab22049714ad8a6ac26236f46090ddecfb
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-24 01:03:17 -0400

    addingn advance task 1 - 2

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Second challenge """
+from typing import Any, Sequence, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """ Use correct duct-typed annotations """
+    if lst:
+        return lst[0]
+    else:
+        return None
