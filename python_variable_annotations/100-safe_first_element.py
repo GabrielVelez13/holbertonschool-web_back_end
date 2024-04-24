e37688117f706a23bd60df42afd7ca6bcd691754
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-24 02:05:54 -0400

    fixing task 100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,8 +3,9 @@
 from typing import Any, Sequence, Optional
 
 
+# The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    """ Duck typing checking if its length """
+    """ adding comment for testing """
     if lst:
         return lst[0]
     else:
