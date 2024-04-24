60db47af6d267f7835278be7240d522c0cc5a8e4
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-24 02:03:25 -0400

    fixing task 100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" Second challenge """
+""" Second challenge checing if its length"""
 from typing import Any, Sequence, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    """ Duck typing """
+    """ Duck typing checking if its length """
     if lst:
         return lst[0]
     else:
--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Third challenge """
+""" Third challenge checking if it is length """
 from typing import Mapping, Union, Any, TypeVar
 
 T = TypeVar('T')
@@ -7,7 +7,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
-    """ Using typevar """
+    """ Using typevar Checking if it is length """
     if key in dct:
         return dct[key]
     else:
