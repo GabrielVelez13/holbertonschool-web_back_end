f23d3934e627b52661e8232abd5f89e6c84a77b2
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-24 01:55:53 -0400

    fixing task 100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Second challenge """
-from typing import Any, Sequence, Optional, List
+from typing import Any, Sequence, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    if lst is List:
+    if lst:
         return lst[0]
     else:
         return None
