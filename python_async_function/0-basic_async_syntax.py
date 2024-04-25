6282079ea81931fe0f9acb04093912b3c0a0ecfd
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-25 16:41:21 -0400

    adding task 3

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ First asynchronous coroutine """
 import asyncio
-import random
-
+from random import uniform
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Random wait time to test async """
-    wait_time = random.uniform(0, max_delay)
+    wait_time = uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time
