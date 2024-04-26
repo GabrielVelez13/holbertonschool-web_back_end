8a1cfcd0b3f1a66906bf3d8bed5f8863da1835ee
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-26 18:15:40 -0400

    fixing tasks

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,5 +5,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List:
-    """ Async comprehension """
+    """ Async comprehension the flesh """
     return [num async for num in async_generator()]
