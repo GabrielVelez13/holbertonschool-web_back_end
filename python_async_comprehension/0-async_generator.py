0488b40f69da33f24ef7df6910592b6642b219b6
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-26 17:57:13 -0400

    fixing task 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import Iterable
 
 async def async_generator() -> Iterable:
     """ Yield a random number from 0 to 10 """
-    for _ in range(10):
+    for i in range(10):
         await sleep(1)
         yield uniform(0, 10)
