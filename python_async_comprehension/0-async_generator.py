91472b241a6d07280cd0e25ca17116cfce10ef64
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-26 18:16:23 -0400

    fixing tasks

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ Yield a random number from 0 to 10 """
     for _ in range(10):
         await sleep(1)
