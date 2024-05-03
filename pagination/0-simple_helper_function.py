dd7f468add0e43d7de8ab927db2d277a631fd7c8
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-05-03 16:17:44 -0400

    adding task 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """ Simple helper for pagination """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple:
     """ Returns a tuple with the start and end of the current page """
     return (page_size*page - page_size, page_size*page)
