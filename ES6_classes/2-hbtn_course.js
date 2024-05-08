9c2eed94b8b8201d212fdcc8c2195f7a07a38409
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-05-08 16:23:38 -0400

    fixing task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,52 +1,52 @@
 export default class HolbertonCourse {
-  constructor(name, length, students){
-    this._name = this._verifyString(name, 'Name');
-    this._length = this._verifyNumber(length, 'Length');
-    this._students = this._verifyArray(students, 'Students');
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set name(newName) {
-    this._name = this._verifyString(newName, 'Name');
-  }
-
-  set length(newLength) {
-    this.length = this._verifyNumber(newLength, 'Length');
-  }
-
-  set students(newStudents) {
-    this._students = this._verifyArray(newStudents, 'Students');
-  }
-
-  _verifyString(value, attribute) {
-    if(typeof value !== 'string') {
-      throw TypeError(`${attribute} must be a string`);
+    constructor(name, length, students){
+      this._name = this._verifyString(name, 'Name');
+      this._length = this._verifyNumber(length, 'Length');
+      this._students = this._verifyArray(students, 'Students');
     }
-    return value;
-  }
 
-  _verifyNumber(value, attribute) {
-    if (typeof value !== 'number' || isNaN(value)) {
-      throw TypeError(`${attribute} must be a number`);
+    get name() {
+      return this._name;
     }
-    return value;
-  }
 
-  _verifyArray(value, attribute) {
-    if (!Array.isArray(value)) {
-      throw TypeError(`${attribute} must be an array of strings`);
+    get length() {
+      return this._length;
     }
-    return value;
+
+    get students() {
+      return this._students;
+    }
+
+    set name(newName) {
+      this._name = this._verifyString(newName, 'Name');
+    }
+
+    set length(newLength) {
+      this._length = this._verifyNumber(newLength, 'Length');
+    }
+
+    set students(newStudents) {
+      this._students = this._verifyArray(newStudents, 'Students');
+    }
+
+    _verifyString(value, attribute) {
+      if(typeof value !== 'string') {
+        throw new TypeError(`${attribute} must be a string`);
+      }
+      return value;
+    }
+
+    _verifyNumber(value, attribute) {
+      if (typeof value !== 'number' || isNaN(value)) {
+        throw new TypeError(`${attribute} must be a number`);
+      }
+      return value;
+    }
+
+    _verifyArray(value, attribute) {
+      if (!Array.isArray(value) || value.some((student) => typeof student !== 'string')) {
+        throw new TypeError(`${attribute} must be an array of strings`);
+      }
+      return value;
+    }
   }
-}
