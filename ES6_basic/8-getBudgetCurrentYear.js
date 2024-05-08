11520d0ecd0c49653fd4c7652a2b93e35c432636
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-05-07 20:48:45 -0400

    testing lint

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,15 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {};
+  const budget = {};
 
-    const year = getCurrentYear();
-    budget[`income-${year}`] = income;
-    budget[`gdp-${year}`] = gdp;
-    budget[`capita-${year}`] = capita;
+  const year = getCurrentYear();
+  budget[`income-${year}`] = income;
+  budget[`gdp-${year}`] = gdp;
+  budget[`capita-${year}`] = capita;
 
-    return budget;
+  return budget;
 }
